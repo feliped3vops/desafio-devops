23fabbcc8cc7710f1b806504d2e6655ba5331210
Felipe Ferreira Gomes <felipetech25@gmail.com>
2026-09-01 19:57:58 -0300

    fix: corrige tratamento das consultas ao banco

--- node/routes.js
+++ node/routes.js
@@ -7,17 +7,28 @@ const connection = require('./connectionDb');
 
 routes.get('/', (_, res) => {
     const sql = `INSERT INTO peoples(name) VALUES('${faker.name.findName()}')`;
-    connection.query(sql);
 
-    connection.query("SELECT * FROM peoples", (_, results) => {
-        let html = '<h1>Desafio Devops!</h1>';
+    connection.query(sql, (err) => {
+        if (err) {
+            console.error('Erro ao inserir registro:', err);
+            return res.status(500).send('Erro ao inserir registro');
+        }
 
-        results.forEach(element => {
-            html += element.name + '<br>'
-        })
+        connection.query("SELECT * FROM peoples", (err, results) => {
+            if (err) {
+                console.error('Erro ao consultar registros:', err);
+                return res.status(500).send('Erro ao consultar registros');
+            }
 
-        return res.send(html);
+            let html = '<h1>Desafio Devops!</h1>';
+
+            results.forEach(element => {
+                html += element.name + '<br>';
+            });
+
+            return res.send(html);
+        });
     });
-})
+});
 
 module.exports = routes;
