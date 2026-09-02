cad9d288d722cd7e148e3f23873ccc073a5f6971
Felipe Ferreira Gomes <felipetech25@gmail.com>
2026-09-02 16:31:00 -0300

    feat: adiciona logs de requisicoes da aplicacao

--- node/index.js
+++ node/index.js
@@ -4,6 +4,20 @@ const routes = require('./routes')
 const app = express()
 const port = 3000
 
+app.use((req, res, next) => {
+    const start = Date.now();
+
+    res.on('finish', () => {
+        const duration = Date.now() - start;
+
+        console.log(
+            `${req.method} ${req.originalUrl} ${res.statusCode} ${duration}ms`
+        );
+    });
+
+    next();
+});
+
 app.use(routes);
 
 app.listen(port, () => {
